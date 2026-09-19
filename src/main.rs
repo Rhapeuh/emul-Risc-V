b3afad1ea80aa31863bb93edbc0e34c31fe0d956
Rhapeuh <ylannwattrelos060@gmail.com>
2026-09-19 19:00:12 +0200

    fin du rendu 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, fs::File, io::{self, Read}};
+
+fn main() -> io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("Pas assez d'argument");
+        std::process::exit(1);
+    }
+
+    let chemin_fichier = &args[1];
+    let mut fichier = File::open(chemin_fichier)?;
+
+    let mut buffer = [0u8; 4];
+    let mut offset = 0;
+
+    println!("offset,valeur,opcode,encoding");
+
+    while fichier.read_exact(&mut buffer).is_ok() {
+        let instruction = u32::from_le_bytes(buffer);
+
+        let offset_hexa = format!("{:08x}", offset);
+        let instruction_hexa = format!("{:08x}", instruction);
+
+        let opcode = instruction & 0x7F;
+
+        let (opcode_string, encodage) = match opcode {
+            0b1100011 => ("BRANCHE", "S_B"),
+            0b1100111 => ("JALR", "I"),
+            0b0000011 => ("LOAD", "I"),
+            0b0001111 => ("MISC-MEM", "I"),
+            0b0010011 => ("OP-IMM", "I"),
+            0b1110011 => ("SYSTEM", "I"), 
+            0b1101111 => ("JAL", "U_J"),
+            0b0110011 => ("OP", "R"),
+            0b0100011 => ("STORE", "S"),
+            0b0010111 => ("AUIPC", "U"),
+            0b0110111 => ("LUI", "U"),
+            _ => ("INCONNU", "INCONNU"),
+        };
+
+        println!("{},{},{},{}", offset_hexa, instruction_hexa, opcode_string, encodage);
+
+        offset += 4;
+    }
+
+    Ok(())
 }
